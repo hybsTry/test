b531560527897f8c1e4945ddf5202c7d05bd8b95
hybsTry <haya.y1423@gmail.com>
2023-05-19 11:50:18 +0900

    ファーストコミット

---
+++ hosuu.php
@@ -0,0 +1,27 @@
+<?php
+function calculateSteps($distance) {
+    // 歩数計算のロジックを実装する
+    // 例: 1メートルあたりの歩数を設定して、総距離を歩数に変換する
+    $stepsPerMeter = 0.75; // 仮の値です。実際の値に置き換えてください。
+    $steps = $distance * $stepsPerMeter;
+
+    return $steps;
+}
+
+// マップのルートの距離（メートル）を取得（API取得のロジックを実装する必要があります）
+$routeDistance = getRouteDistanceFromAPI(); // マップAPIからルートの距離を取得する関数を呼び出す
+
+// 歩数を計算
+$steps = calculateSteps($routeDistance);
+
+// 結果を表示
+echo "ルートの距離: {$routeDistance}メートル<br>";
+echo "歩数: {$steps}歩";
+
+// マップAPIからルートの距離を取得する関数の例
+function getRouteDistanceFromAPI() {
+    // マップAPIを使用してルートの距離を取得する処理を実装する
+    // ここでは仮の値として5000メートルを返す
+    return 5000;
+}
+?>
